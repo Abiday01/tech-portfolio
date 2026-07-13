d4b281d93e265ada94bc47193fbf61f00565d8d7
Abida <136748161+Abiday01@users.noreply.github.com>
2026-07-13 13:40:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -116,3 +116,20 @@ document.addEventListener('DOMContentLoaded', () => {
     updateSlider();
 
 });
+
+
+
+
+
+
+
+
+
+const menuBtn = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
